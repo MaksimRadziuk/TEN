387bd8f04902f2e23159ac909640262f0149a297
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-01-17 23:18:18 +0300

    17/01 23.20

--- TEN-policy/template/js/common.js
+++ TEN-policy/template/js/common.js
@@ -3,14 +3,6 @@ $(document).ready(function(){
   var backgroundSize = $('body').height();
   $('#popup-background').css('height', backgroundSize);
 
-if(innerWidth>550){
-	var widthPopup = $('.popup').width();
-   var leftPadingPopup = widthPopup/2;
-   $('.popup').css('marginLeft', -leftPadingPopup);
-}
-  
-else {}
-
   $('.cabinet-box').click(function(){
   	$('#popup-background').fadeIn();
   	$('#popup-entrance').show();
