ff205d8247b4397f7bdb93fdac306cd461cb3305
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-02-05 00:29:33 +0300

    5/02 0.30

---
+++ TEN-test/template/js/test.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	/*if (innerWidth < 421) {
+		var h1 = $('.test_content_block.two .test_content_block.two .questions_wrap .questions_column.first_column').innerHeight();
+		var h2 = $('.test_content_block.two .test_content_block.two .questions_wrap .questions_column.second_column').innerHeight();
+		var hSum = h1 + h2;
+		$('.test .test_content_block.two .questions_wrap').css('height', hSum);
+	}
+	else{}*/
+
+});
